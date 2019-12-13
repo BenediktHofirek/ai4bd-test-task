1c9372ea51a879f12cba0bfee675addac16a284f
utility4u <benedikthofirek@gmail.com>
2019-12-13 05:44:54 +0100

    refactoring

--- server/app.js
+++ server/app.js
@@ -7,15 +7,15 @@ const cors = require('cors');
 const app = express();
 
 mongoose
-	// .connect('mongodb://silas:BJJjtTPyUV6XPd7x@94.130.203.236:27022/doc_viewer', {
-	// 	authSource: 'doc_viewer',
-	// 	useNewUrlParser: true,
-	// 	useUnifiedTopology: true
-	// })
-	.connect('mongodb://127.0.0.1:27017/test', {
+	.connect('mongodb://silas:BJJjtTPyUV6XPd7x@94.130.203.236:27022/doc_viewer', {
+		authSource: 'doc_viewer',
 		useNewUrlParser: true,
 		useUnifiedTopology: true
 	})
+	// .connect('mongodb://127.0.0.1:27017/test', {
+	// 	useNewUrlParser: true,
+	// 	useUnifiedTopology: true
+	// })
 	.then(() => console.log('connection successful'))
 	.catch((err) => console.error(err));
 
